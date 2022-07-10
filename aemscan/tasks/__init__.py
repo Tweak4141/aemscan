29814f37b5137c1b76074e9868f49c376ab13a70
Tweak <ynan243@gmail.com>
2022-07-10 05:06:34 -0600

    Update __init__.py

--- aemscan/tasks/__init__.py
+++ aemscan/tasks/__init__.py
@@ -1,14 +1,12 @@
 __author__ = 'raz0r'
 
 from . import \
-    get_version, \
     check_default_error_page, \
     check_webdav, \
     scan_useful_paths, \
     bruteforce_default_credentials
 
 default_tasks = [
-    get_version,
     check_default_error_page,
     check_webdav,
     scan_useful_paths,
